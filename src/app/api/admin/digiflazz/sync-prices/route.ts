abb66a744ce84633c084be7e65c7760153ac9c3a
imjvlian <julianvikbusiness@gmail.com>
2026-08-31 10:45:42 +0700

    0.2.2

--- src/app/api/admin/digiflazz/sync-prices/route.ts
+++ src/app/api/admin/digiflazz/sync-prices/route.ts
@@ -159,7 +159,6 @@ export async function POST(request: Request) {
                 supplier_id: "eq.digiflazz",
                 product_id: `eq.${result.productId}`,
               },
-              prefer: "return=minimal",
             },
           ),
         ),
